412d3df2ea46e25cb5f73e86bcc0247799d2a3d4
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-08-25 02:36:04 +0200

    Create scriptideas1.bat

---
+++ scriptideas1.bat
@@ -0,0 +1,47 @@
+color 80
+title Scripter v0.1 by: M. Lund
+::imagetoascii text-image.com
+@echo off
+:choice
+cls
+set /P choice=">: "
+IF %choice% equ santa goto santa
+IF %choice%==9 goto script
+IF %choice%=="" goto exit
+
+:exit
+exit
+
+:santa
+exit
+
+:script
+cls
+echo                    `.........`                   
+echo                `..-..-----------.                
+echo              `.-----:://///:::::::.              
+echo             `----://:-....-://////+:`            
+echo            .::-::/-``      ``-/++++o:`           
+echo           `:::::/-`          `.+oooos:`          
+echo          `-:::///`            `-osyyys.          
+echo          `://////`             .+yyyyy/`         
+echo          `:////+/`             `/yyyhho`         
+echo          `-/+++++.             .oyyyhhy.         
+echo           `/++++++.           `/yyhhhhh.         
+echo           `./++ooo+-`      `.:oyhhhhhhh.         
+echo             `:osssso+/::::/+oyhyshhhddy.         
+echo              `.:+syyyyyyyyyhhs/-ohddddo`         
+echo                ``.:/+ooooo+/-``.sddddd:`         
+echo                    ````````   `:hdddds.          
+echo                               .sddddh-`          
+echo                              `ohdddd/`           
+echo                            `-ohdddh/`            
+echo             .+/-..``   ``.:+yhdddy-`             
+echo            `-yysssoooooosyhhdddy/.`              
+echo            `-syhhhhhdddddddhy+-.`                
+echo             ``.--://+++//:-.``                   
+echo                     ```                          
+::pause for 2 sec
+ping 127.0.0.1 -n 2 > nul
+pause
+exit
